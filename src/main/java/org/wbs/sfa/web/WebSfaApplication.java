03891b1050fa3b8b604e66ee47beaf93b097f02f
alex <alex@virowater.com>
2016-07-11 14:52:00 +0700

    # WARNING: head commit changed in the meantime
    Original Delivery

---
+++ src/main/java/org/wbs/sfa/web/WebSfaApplication.java
@@ -0,0 +1,12 @@
+package org.wbs.sfa.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSfaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSfaApplication.class, args);
+	}
+}
